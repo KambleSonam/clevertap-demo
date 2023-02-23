0a2b13e2f68ad837b2868eb77d143805e7527a52
KambleSonam <98796470+KambleSonam@users.noreply.github.com>
2023-02-23 13:41:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import clevertap from 'clevertap-web-sdk';
 
 
-// clevertap.init('W9R-486-4W5Z')
-clevertap.init('4R4-5K9-475Z', 'sk1-staging-13', 'wzrkt.com')
+clevertap.init('W9R-486-4W5Z')
+// clevertap.init('4R4-5K9-475Z', 'sk1-staging-13', 'wzrkt.com')
 clevertap.setLogLevel(3)
 clevertap.privacy.push({ optOut: false, useIP: true });
 clevertap.dismissSpamControl = true
