3e0f264200b08c3f93aa5e0dcc5ce4819227b116
Sonam <sonam@clevertap.com>
2026-01-30 12:25:20 +0530

    staging account

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import clevertap from 'clevertap-web-sdk';
 // clevertap.enableCustomCleverTapId = true
 
 // console.log('before', clevertap.enableCustomCleverTapId)
-clevertap.init('W9R-486-4W5Z', '', '', '', {enableEncryptionInTransit: true})              // Clevertap Sample
+// clevertap.init('W9R-486-4W5Z', '', '', '', {enableEncryptionInTransit: true})              // Clevertap Sample
 // clevertap.init('K99-655-Z96Z', '', '', '556-cc3')              // Demo Ecommerce
 
 // clevertap.init('WWW-WWW-WWRZ')              // Bearded Robot eu1
@@ -21,7 +21,7 @@ clevertap.init('W9R-486-4W5Z', '', '', '', {enableEncryptionInTransit: true})   
 
 // clevertap.init('W48-7W7-495Z', 'sk1-staging-4', 'wzrkt.com')      // QA Warrior
 // clevertap.init('WRK-485-456Z', 'sk1-staging-4', 'wzrkt.com')      // Web Testing
-// clevertap.init('884-5ZW-8Z7Z', 'sk1-staging-4', 'wzrkt.com')      // noc internal
+clevertap.init('884-5ZW-8Z7Z', 'sk1-staging-4', 'wzrkt.com', '', {enableEncryptionInTransit: true})      // noc internal
 // clevertap.init('RZK-797-6Z7Z', 'sk1-staging-4', 'wzrkt.com')      // Test-Sonam
 // clevertap.init('WRK-485-456Z', 'sk1-staging-16', 'wzrkt.com')      // Web Testing
 // clevertap.init('6Z8-875-R85Z', 'us1', '', '5ab-b16')
